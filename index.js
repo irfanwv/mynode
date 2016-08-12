3a02b8bd97a9ea85f27ab29f1cc5d329afce5e25
irfanwv <Mars-One@iPhones-Mac-Pro-6.local>
2016-08-12 18:08:55 +0530

    commit3

--- index.js
+++ index.js
@@ -1,57 +1,20 @@
-// server.js
-// load the things we need
 var express = require('express');
 var app = express();
 
-var bodyParser = require('body-parser');
+app.set('port', (process.env.PORT || 5000));
 
-// Create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
-// set the view engine to ejs
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-
-// index page 
-//app.get('/', function(req, res) {
-//    res.render('pages/index');
-//});
-
-app.get('/', function(req, res) {
-    var drinks = [
-        { name: 'Bloody Mary', drunkness: 3 },
-        { name: 'Martini', drunkness: 5 },
-        { name: 'Scotch', drunkness: 10 }
-    ];
-    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else.";
-
-    res.render('pages/index', {
-        drinks: drinks,
-        tagline: tagline
-    });
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-// about page 
-app.get('/about', function(req, res) {
-    res.render('pages/about');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
 
-// about page 
-app.get('/contact', function(req, res) {
-    res.render('pages/contact');
-});
 
-app.post('/contact', urlencodedParser, function (req, res) {
-
-   // Prepare output in JSON format
-   response = {
-       first_name:req.body.first_name,
-       last_name:req.body.last_name
-   };
-   console.log(response);
-   res.end(JSON.stringify(response));
-})
-
-
-app.listen(8080);
-console.log('8080 is the magic port');
