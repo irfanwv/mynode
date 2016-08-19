2fc91a827058a1512be2d8b87f71197dff4ae827
irfanwv <Mars-One@iPhones-Mac-Pro-6.local>
2016-08-19 18:09:19 +0530

    update7

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var nodemailer = require("nodemailer");
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -9,10 +10,42 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
+
+/*
+    Here we are configuring our SMTP Server details.
+    STMP is mail server which is responsible for sending and recieving email.
+*/
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service: "Gmail",
+    auth: {
+        user: "james01.matthew@gmail.com",
+        pass: "success@123"
+    }
+});
+/*------------------SMTP Over-----------------------------*/
+
+/*------------------Routing Started ------------------------*/
 app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/send',function(req,res){
+    var mailOptions={
+        to : req.query.to,
+        subject : req.query.subject,
+        text : req.query.text
+    }
+    console.log(mailOptions);
+    smtpTransport.sendMail(mailOptions, function(error, response){
+     if(error){
+            console.log(error);
+        res.end("error");
+     }else{
+            console.log("Message sent: " + response.message);
+        res.end("sent");
+         }
+});
+});/*--------------------Routing Over----------------------------*/
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
